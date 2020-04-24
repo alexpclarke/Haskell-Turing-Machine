023033d974c5001708d18de2bd45c12bf81dacb2
Alexander Clarke <alex-p-clarke@hotmail.com>
2020-04-24 16:30:39 -0400

    working on tape

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -1,11 +1,12 @@
 type State = Int
 type Symbol = Maybe Int
 
-data Move = Idle | Left | Right
+data Move = Idle | MoveLeft | MoveRight
 data Transition = Transition State Symbol State Symbol Move
-data Tape = Tape [Symbol] Symbol [Symbol]
+data Tape = Tape [Symbol] Symbol [Symbol] deriving (Show)
 data TuringMachine = TuringMachine [Transition] State Tape
 
+-- Main
 main = do
   putStrLn "-- Haskell Turing Machine --"
   putStrLn "by: Alex Clarke and Gabrielle Maxwell\n"
@@ -13,3 +14,14 @@ main = do
   fileName <- getLine
   fileContents <- readFile fileName
   putStrLn fileContents
+
+-- Helper functions
+
+checkState :: State -> [State] -> Bool
+checkState s [] = False
+checkState s (a:xa) = if (s == a) then True else checkState s xa
+
+moveTape :: Tape -> Move -> Tape
+moveTape t Idle = t
+moveTape (l, x, (r:rs)) MoveRight = Tape x:l r rs
+-- moveTape ((l:ls), x, r) MoveLeft = Tape ls l x:r
