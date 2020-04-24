9682f74dfc8269e99cd197a0d104409ec81593ac
Alexander Clarke <alex-p-clarke@hotmail.com>
2020-04-24 15:18:53 -0400

    started adding main

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -5,3 +5,10 @@ data Move = Idle | Left | Right
 data Transition = Transition State Symbol State Symbol Move
 data Tape = Tape [Symbol] Symbol [Symbol]
 data TuringMachine = TuringMachine [Transition] State Tape
+
+main = do
+  putStrLn "-- Haskell Turing Machine --"
+  putStrLn "by: Alex Clarke and Gabrielle Maxwell\n"
+  putStrLn "Input file name:"
+  fileName <- getLine
+  putStrLn $ "the input file is " ++ fileName
