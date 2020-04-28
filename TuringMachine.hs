6e7e2fc96640c81ad96d4acf1b5231014ed976b5
Alexander Clarke <alex-p-clarke@hotmail.com>
2020-04-28 13:05:49 -0400

    cleaning things up and making them more concise

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -1,11 +1,11 @@
-type State = Int
-type Symbol = Int
+type State = String
+type Symbol = String
 
 data Move = Idle | MoveLeft | MoveRight
+data Status = Accept | Fail | Running
 data Transition = Transition {
   input :: (State, Symbol),
-  output :: (State, Symbol),
-  move :: Move
+  output :: (State, Symbol, Move)
 }
 data Tape = Tape {
   leftSide :: [Symbol],
@@ -28,48 +28,32 @@ main = do
   fileContents <- readFile fileName
   putStrLn fileContents
 
--- Helper functions
-
-isAccept :: TuringMachine -> Bool
-isAccept (TuringMachine _ s _ []) = False
-isAccept (TuringMachine ts s t (a:as)) =
-  if (s == a)
-  then True
-  else isAccept (TuringMachine ts s t as)
-
-moveTape :: Tape -> Move -> Tape
-moveTape t Idle = t
-moveTape (Tape l x (r:rs)) MoveRight = Tape (x:l) r rs
-moveTape (Tape (l:ls) x r) MoveLeft = Tape ls l (x:r)
-
 run :: TuringMachine -> TuringMachine
-run tm =
-  if (isAccept tm)
-  then tm
-  else run (step tm)
+run tm = if (isAccept tm) then tm else run (step tm)
 
 step :: TuringMachine -> TuringMachine
-step (TuringMachine ts s (Tape l x r) as) =
-  let t = findTransition ts s x
-  in TuringMachine ts (getTOSt t) (moveTape (Tape l (getTOSy t) r) (getTOM t)) as
+step (TuringMachine ts st0 (Tape l sy0 r) as) =
+  let (st1, sy1, m) = findTransition ts (st0, sy0)
+  in TuringMachine ts st1 (moveTape (Tape l sy1 r) m) as
 
-findTransition :: [Transition] -> State -> Symbol -> Transition
--- if transition is not found, the machine fails
-findTransition (t:ts) st sy
-  | ((getTISt t) == st && (getTISt t) == sy) = t
-  | otherwise = findTransition ts st sy
 
-getTISt :: Transition -> State
-getTISt (Transition (x, _) (_, _) _) = x
+-- Helper Functions
 
-getTISy :: Transition -> Symbol
-getTISy (Transition (_, x) (_, _) _) = x
+isAccept :: TuringMachine -> Bool
+isAccept (TuringMachine _ st _ []) = False
+isAccept (TuringMachine ts st ta (a:as))
+  | st == a = True
+  | otherwise = isAccept (TuringMachine ts st ta as)
 
-getTOSt :: Transition -> State
-getTOSt (Transition (_, _) (x, _) _) = x
+moveTape :: Tape -> Move -> Tape
+moveTape ta Idle = ta
+moveTape (Tape l sy (r:rs)) MoveRight = Tape (sy:l) r rs
+moveTape (Tape (l:ls) sy r) MoveLeft = Tape ls l (sy:r)
 
-getTOSy :: Transition -> Symbol
-getTOSy (Transition (_, _) (_, x) _) = x
+findTransition :: [Transition] -> (State, Symbol) -> (State, Symbol, Move)
+findTransition ((Transition input output):ts) s
+  | input == s = output
+  | otherwise = findTransition ts s
 
-getTOM :: Transition -> Move
-getTOM (Transition (_, _) (_, _) x) = x
+getInput :: Transition -> (State, Symbol)
+getInput (Transition x _) = x
